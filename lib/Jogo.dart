c7e89c9e875fc39cc43cccac394d4883c1cccb1f
jeeeanlefchak <jean_2103@hotmail.com>
2019-09-24 13:05:20 -0300

    criaando o joken po

---
+++ lib/Jogo.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Jogo extends StatefulWidget {
+  @override
+  _JogoState createState() => _JogoState();
+}
+
+class _JogoState extends State<Jogo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+            "JokenPo"
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16),
+            child: Text(
+              "Escolha do app",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+          Image.asset("imagens/padrao.png"),
+        ],
+      ),
+    );
+  }
+}
